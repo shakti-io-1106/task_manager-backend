a07d3e211415b415bdc6ac7d076926bbb50774a1
shakti-io-1106 <shaktipada.sps@gmail.com>
2026-05-16 17:27:26 +0530

    User Authentication for Register and Login.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,43 +1,80 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import generateToken from "../utils/generateToken.js";
 
-export const registerUser = async (req,res)=>{
-    try{
-        const {name,email,password} = req.body;
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-        if(!name || !email || !password){
-            return res.status(400).json({
-                message:"All Fields are required.",
-            });
-        }
-        
-        const existsUser = await User.findOne({email});
-
-        if(existsUser){
-            return res.status(400).json({
-                message:"User Already Exists."
-            })
-        }
-
-        //Hashing
-        const salt = bcrypt.genSalt(10);
-
-        const hashedPassword = await bcrypt.hash(password,salt);
-
-        //Creating User
-        const user = await User.create({
-            name,email,password:hashedPassword
-        });
-
-        res.status(201).json({
-            _id:user._id,
-            name:user.name,
-            email:user.email,
-            message:"User Created Successfully";
-        });
-    }catch(error){
-        res.ststus(500).json({
-            message:error.message
-        });
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: "All Fields are required.",
+      });
     }
+
+    const existsUser = await User.findOne({ email });
+
+    if (existsUser) {
+      return res.status(400).json({
+        message: "User Already Exists.",
+      });
+    }
+
+    //Hashing
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //Creating User
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      message: "User Created Successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    //Check User
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    //Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
+    }
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
